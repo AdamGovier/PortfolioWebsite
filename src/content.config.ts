ab343891b08c652a1be31a0255225012b13feaf9
Adam GOvier <adam.govier@dev4.online>
2026-09-22 22:47:10 +0100

    Added basic seo

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,13 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
 
-import { glob, file } from 'astro/loaders';
-import { type BlogPostMetadata } from './types/collection';
+import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
+    description: z.string(),
     pubDate: z.date(),
     slug: z.string(),
     image: z
@@ -37,7 +38,7 @@ const blog = defineCollection({
         })
       )
       .optional(),
-  }) as z.ZodType<BlogPostMetadata>,
+  }),
 });
 
 export const collections = { blog };
--- src/models/Head.ts
+++ src/models/Head.ts
@@ -1,6 +1,11 @@
 export interface SearchEngineMetaData {
-  title?: string;
-  description?: string;
+  title: string;
+  description: string;
   keywords?: string[];
   author?: string;
+  image?: string;
+  imageAlt?: string;
+  type?: "website" | "article";
+  publishedTime?: Date | string;
+  noindex?: boolean;
 }
